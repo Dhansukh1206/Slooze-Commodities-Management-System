aa2f49bee69255b76d8443ffdcab9a0edcf20228
Dhansukh1206 <dhansukh3860@gmail.com>
2026-04-06 10:37:02 +0530

    refactor: memoize Apollo client initialization using useState in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import { ThemeProvider } from "next-themes";
 import { ApolloProvider } from "@apollo/client/react";
 import { getApolloClient } from "@/lib/apollo-client";
 import { AuthProvider } from "@/contexts/AuthContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const client = getApolloClient();
+  const [client] = useState(() => getApolloClient());
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
